cc8faf28d6afb71ccde715d75ce0b7ae3f2fc35d
jessica-calderon <calderonjessica13@yahoo.com>
2022-07-03 01:05:01 -0500

    update questions array

--- index.js
+++ index.js
@@ -1,24 +1,124 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
-const { writeFile, copyFile } = require('./utils/generateMarkdown');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+const fs = require('fs');
+const { inherits } = require('util');
 
-const promptUser = () => {
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: 'What is your name? (Required)',
-            validate: nameInput => {
-                if (nameInput) {
-                    return true;
-                } else {
-                    console.log('Please enter your name!');
-                    return false;
-                }
-            }
-        }
-    ])
-}
+// questions array 
+const questionsArr = [
+  {
+    type: 'input',
+    name: 'github',
+    message: 'What is your GitHub username? (Required)',
+    validate: githubInput => {
+      if (githubInput) {
+        return true;
+      } else {
+        console.log('Please enter your GitHub username!');
+        return false;
+      }
+    }
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address? (Required)',
+    validate: emailInput => {
+      if (emailInput) {
+        return true;
+      } else {
+        console.log('Please enter your email address!');
+        return false;
+      }
+    }
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'What is your GitHub username? (Required)',
+    validate: githubInput => {
+      if (githubInput) {
+        return true;
+      } else {
+        console.log('Please enter your GitHub username!!');
+        return false;
+      }
+    }
+  },
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project? (Required)',
+    validate: titleInput => {
+      if (titleInput) {
+        return true;
+      } else {
+        console.log('Please enter the title of your project!');
+        return false;
+      }
+    }
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Please enter a short description of your project. (Required)',
+    validate: descriptionInput => {
+      if (descriptionInput) {
+        return true;
+      } else {
+        console.log('Please enter a description of your project!');
+        return false;
+      }
+    }
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'What kind of license does your project have?',
+    choices: ['agpl', 'Apache', 'MIT', 'No license']
+   },
+   {
+    type: 'confirm',
+    name: 'contributors',
+    message: 'Do you want to allow other developers to contribute?',
+    default: true
+   },
+   {
+    type: 'input',
+    name: 'contribute',
+    message: 'Please provide contribution guidelines. (Required)',
+    when: ({ contribute }) => {
+      if (contribute) {
+          return true;
+      } else {
+          return false;
+      }
+  },
+  validate: contributorInput => {
+      if (contributorInput) {
+          return true;
+      } else {
+          console.log("Please enter your project's contributor guidelines!");
+          return false;
+      }
+  }
+},  
+{
+  type: 'input',
+  name: 'install',
+  message: 'What are the steps required to install your project?',
+  validate: nameInput => {
+      if (nameInput) {
+          return true;
+      } else {
+          console.log('Please enter steps required to install your project!');
+          return false; 
+      }
+  }
+},
+
+]
+
 // TODO: Create an array of questions for user input
 /* const questions = []; */
 
@@ -30,4 +130,6 @@ const promptUser = () => {
 
 // Function call to initialize app
 /* init(); */
-promptUser()
+const init = () => {
+  
+}
